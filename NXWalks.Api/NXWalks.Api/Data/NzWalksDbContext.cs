ecc06968acd445cb5f711897b143ef70258c971b
karuna <karunapandey125@gmail.com>
2023-08-24 20:57:24 +0545

    Models Created

---
+++ NXWalks.Api/NXWalks.Api/Data/NzWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NXWalks.Api.Models.Domain;
+
+namespace NXWalks.Api.Data
+{
+    public class NzWalksDbContext:DbContext
+    {
+        public NzWalksDbContext(DbContextOptions<NzWalksDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+
+
+    }
+}
---
+++ NXWalks.Api/NXWalks.Api/Models/Domain/Walk.cs
@@ -0,0 +1,14 @@
+﻿namespace NXWalks.Api.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+        //NavigationProperty 
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
